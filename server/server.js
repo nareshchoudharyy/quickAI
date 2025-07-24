7e975555bf2ffbf198adfd152c903f24938ca061
Naresh Choudhary <nareshgorachiya@gmail.com>
2025-07-24 22:48:33 +0530

    Add health check endpoint to server

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 app.use(clerkMiddleware())
 app.use(requireAuth());
 
+app.get('/', (req, res) => res.send("Server is Live!"))
 app.use('/api/ai', aiRouter)
 app.use('/api/user', userRouter)
-app.get('/ai', (req, res) => res.send("Server is Live!"))
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

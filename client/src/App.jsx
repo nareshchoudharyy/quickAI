5d67a2b4b3fa25ae72358794c5258f4471f3345a
Naresh Choudhary <nareshgorachiya@gmail.com>
2025-07-24 22:13:05 +0530

    Project Completed

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import BlogTitles from './pages/BlogTitles'
+import WriteArticle from './pages/WriteArticle'
+import GenerateImages from './pages/generateImages'
+import RemoveObject from './pages/RemoveObject'
+import RemoveBackground from './pages/RemoveBackground'
+import Community from './pages/Community'
+import ReviewResume from './pages/ReviewResume'
+import { useAuth } from '@clerk/clerk-react'
+import { Toaster } from "react-hot-toast"
+function App() {
+  
+  return (
+    <div>
+      <Toaster />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ai' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='write-article' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+          <Route path='generate-images' element={<GenerateImages />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-object' element={<RemoveObject />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+          <Route path='community' element={<Community />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.mjs
@@ -0,0 +1,13 @@
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4f46e5',
+            },
+        },
+    },
+}
